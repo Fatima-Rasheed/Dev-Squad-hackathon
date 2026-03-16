3c3d7ec6a4d8bd1c34ed3f830f0d777c1c9d09fe
Fatima Rasheed <fatimarasheedch@gmail.com>
2026-03-16 04:31:27 -0700

    add

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,6 @@ const PORT = process.env.PORT || 5000;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`https://week3-hackathon-iota.vercel.app/`);
   });
 });
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://week-3-hackathon.vercel.app',
 });
 
 API.interceptors.request.use((config) => {
